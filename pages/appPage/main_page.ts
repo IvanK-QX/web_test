430bce7e104a506d34db0e5da06722892600bedc
IvanK-QX <ivan.k@unitedtech.ai>
2023-11-21 17:39:42 +0200

    Add search tests

--- pages/appPage/main_page.ts
+++ pages/appPage/main_page.ts
@@ -1,11 +1,15 @@
-import { Page } from "@playwright/test"
+import { Locator, Page } from "@playwright/test"
 import { apiUrl } from "../../utils/apiUrl"
 
 export class AppMainPage {
     page: Page
+    searchField: Locator
+    searchContent: Locator
 
     constructor(page: Page) {
         this.page = page 
+        this.searchField = page.locator('[placeholder="Search Everything"]')
+        this.searchContent = page.locator('div.search__content--loaded')
     }
 
     async open() {
@@ -23,6 +27,16 @@ export class AppMainPage {
         await this.page.reload({waitUntil: 'domcontentloaded'})
         await this.page.reload({waitUntil: 'domcontentloaded'})
         this.clickOnStream(streamerName)
+    }
+
+    async searchUserByName(name: string) {
+        await this.searchField.fill(name)
+        await this.page.keyboard.press('Enter')
+        await this.searchContent.waitFor()
+    }
+
+    async validateCorrectSearch(userName = '50cent') {
+        await this.page.locator('div.user-info-card__content--name', {hasText: `userName`}).waitFor()
     }
 
 
